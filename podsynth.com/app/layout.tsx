27c713518ce636483a4e831644f67ab4b33a1527
luhart <ljhartman10@gmail.com>
2024-02-03 22:38:21 -0800

    update

--- podsynth.com/app/layout.tsx
+++ podsynth.com/app/layout.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
