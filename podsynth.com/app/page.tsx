b69fb9204d05de28c8cc55bd1b3d8bd1dd9988e5
luhart <ljhartman10@gmail.com>
2024-02-05 00:47:34 -0800

    update

--- podsynth.com/app/page.tsx
+++ podsynth.com/app/page.tsx
@@ -18,10 +18,20 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start">
       <div className="flex flex-col justify-start max-w-xl min-w-0 w-full p-4 gap-4 pt-12">
-        <HeroText />
-        <div className="text-gray-500 max-w-sm">
-          Podsynth generates mini audio segments or &ldquo;pods&rdquo; for any
-          topics you would like to track.
+        {/* <HeroText /> */}
+        <h1
+          className={`sm:text-2xl text-2xl font-bold flex-1 w-full transition-all duration-500 sm:leading-10 leading-8`}
+        >
+          Turn any feed into a recurring audio digest.
+        </h1>
+        <div className="text-gray-600">
+          Podsynth generates audio summaries straight from your favorite RSS feeds. I think we&apos;re gonna call these &apos;pods&apos;.
+        </div>
+        <div className="text-gray-600">
+          Creating a pod is easy - enter a url, a cadence, press a button, and voila! Now you, too, can listen to summaries of techmeme while brushing your teeth.
+        </div>
+        <div className="text-gray-600">
+          Sign up to experience pure LLM bliss.
         </div>
         <div>
           <Button variant="default" size="lg">
