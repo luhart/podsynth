f628533549e0bd7977b6508d5f931aa62261042c
luhart <ljhartman10@gmail.com>
2024-02-06 23:34:22 -0800

    protect signup

--- podsynth.com/app/signup/page.tsx
+++ podsynth.com/app/signup/page.tsx
@@ -1,6 +1,20 @@
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 import { SignupForm } from "./SignupForm";
 
-export default function SignupPage() {
+export default async function SignupPage() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { session }
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    return redirect('/');
+  }
+
   return (
     <div className="flex flex-col items-center justify-start sm:pt-36 pt-8 min-h-screen w-full">
       <SignupForm />
--- podsynth.com/components/Pods.tsx
+++ podsynth.com/components/Pods.tsx
@@ -6,7 +6,7 @@ export default function Pods() {
   return (
     <div className="max-w-2xl w-full border border-t-0 min-h-screen flex flex-col items-center">
       <div className="flex flex-col justify-start max-w-xl w-full sm:px-4 px-8 gap-4 pt-12">
-        <div className="flex flex-row gap-2 items-center justify-start">
+        <div className="flex flex-row gap-4 items-center justify-start">
           <div className="text-xl font-bold">Pods</div>
           <Button variant="secondary" asChild>
             <Link href="/create" className="flex flex-row gap-1 items-center">
