642dac545b3efb5daf0d6445e5174e7786fa7617
luhart <ljhartman10@gmail.com>
2024-02-15 20:40:35 -0800

    rm extra thing

--- podsynth.com/trigger.ts
+++ podsynth.com/trigger.ts
@@ -1,13 +1,7 @@
 import { TriggerClient } from "@trigger.dev/sdk";
-import { SupabaseManagement } from "@trigger.dev/supabase";
 
 export const client = new TriggerClient({
   id: "podsynth-v1-E7mO",
   apiKey: process.env.TRIGGER_API_KEY,
   apiUrl: process.env.TRIGGER_API_URL,
-});
-
-
-const supabase = new SupabaseManagement({
-  id: "supabase-mgmt-trigger"
 });
