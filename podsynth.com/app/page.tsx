acd14892a28eaccb7bcd5a975df104b07d612429
luhart <ljhartman10@gmail.com>
2024-02-20 23:20:25 -0800

    test

--- podsynth.com/app/page.tsx
+++ podsynth.com/app/page.tsx
@@ -4,14 +4,12 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="flex flex-col justify-start max-w-xl w-full p-4 gap-2 pt-12">
-        {/* <HeroText /> */}
         <h1 className={`text-lg font-bold w-full`}>
-          LLM audio playground
+          All the models
         </h1>
         <div className="text-gray-600">
           Old version is disabled. New version coming soon (in like two days).
         </div>
-        {/* <SampleSection /> */}
       </div>
       <Footer />
     </main>
