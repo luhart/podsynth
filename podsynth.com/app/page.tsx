477c4042fad5ee5266d92d7efc0ccbdf92a7b7e0
luhart <ljhartman10@gmail.com>
2024-02-20 19:41:02 -0800

    remove more

--- podsynth.com/app/page.tsx
+++ podsynth.com/app/page.tsx
@@ -1,35 +1,4 @@
 import { Footer } from "@/components/footer";
-import { Samples } from "@/components/samples";
-import { Button } from "@/components/ui/button";
-import { AudioLines } from "lucide-react";
-import Link from "next/link";
-
-const samples = [
-  {
-    name: "Technology (techmeme rss)",
-    url: "/techmeme-sample.mp3",
-  },
-  {
-    name: "Technology (multi-source)",
-    url: "/tech.mp3",
-  },
-  {
-    name: "Agriculture (agdaily)",
-    url: "/ag-daily.mp3",
-  },
-];
-
-const SampleSection = () => {
-  return (
-    <div className="flex flex-col justify-start max-w-xl min-w-0 w-full gap-4 pt-12">
-      <div className="flex flex-row gap-2 items-center">
-        <AudioLines size={24} />
-        <div className="text-xl font-bold">Samples</div>
-      </div>
-      <Samples samples={samples} />
-    </div>
-  );
-};
 
 export default async function Home() {
   return (
@@ -40,9 +9,9 @@ export default async function Home() {
           LLM audio playground
         </h1>
         <div className="text-gray-600">
-          Old version is disabled. New thing coming soon (in like two days).
+          Old version is disabled. New version coming soon (in like two days).
         </div>
-        <SampleSection />
+        {/* <SampleSection /> */}
       </div>
       <Footer />
     </main>
