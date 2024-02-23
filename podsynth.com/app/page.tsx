32eb9b77dee7ccefb731f96a8061e8f42d6bdd60
luhart <ljhartman10@gmail.com>
2024-02-22 23:00:20 -0800

    update

--- podsynth.com/app/page.tsx
+++ podsynth.com/app/page.tsx
@@ -1,5 +1,7 @@
 import HomeClientAnon from "@/components/HomeClientAnon";
 import { Footer } from "@/components/footer";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default async function Home() {
   return (
@@ -7,13 +9,36 @@ export default async function Home() {
       <div className="max-w-3xl w-full border-l border-r min-h-screen flex flex-col items-center justify-between">
         <div className="flex flex-col justify-start max-w-xl w-full p-4 gap-8 pt-12">
           <div className="flex flex-col gap-2">
-            <h1 className={`text-xl font-bold w-full`}>A playground for language and audio models</h1>
+            <h1 className={`text-xl font-bold w-full`}>
+              A playground for language and audio models.
+            </h1>
             <div className="text-gray-600">
-              This is a place for quickly testing out different mixes of language and audio generation models. It may also turn into a place for people to share and discuss their creations. For now, it&apos;s just a place to play around with some models.
+              This is a place for quickly testing out different mixes of
+              language and audio generation models. Everything in this preview
+              is ephemeral.
             </div>
           </div>
-          <div>Below is a preview. Sign up for the waitlist to be first in line</div>
-          <div className="w-full border-t" />
+          <div className="w-full">
+            <div className="px-4 py-6 border rounded bg-cross max-w-sm w-full">
+              <div className="mb-1 font-medium tracking-tight">
+                This is a preview
+              </div>
+              <div className="text-gray-600 mb-4">
+                Join the waitlist for early access
+              </div>
+              <form className="flex flex-col w-full gap-3">
+                <Input
+                  placeholder="marvin@minsky.com"
+                  type="email"
+                  className="bg-white bg-opacity-50"
+                />
+                <Button variant="default">
+                  Sign up
+                </Button>
+              </form>
+            </div>
+          </div>
+
           <HomeClientAnon />
         </div>
       </div>
