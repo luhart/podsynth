3def52aa71769ccfd95c3b28a8eb3dc8b126abdb
luhart <ljhartman10@gmail.com>
2024-03-12 16:03:31 -0700

    btn

--- podsynth.com/components/workflow/NewBlock.tsx
+++ podsynth.com/components/workflow/NewBlock.tsx
@@ -106,7 +106,7 @@ export function NewBlockCombobox() {
     return (
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-          <Button size="lg" variant="outline">
+          <Button size="lg" variant="outline" className="w-full">
             Add block <Plus className="ml-1 w-4 h-4" />
           </Button>
         </PopoverTrigger>
@@ -120,7 +120,7 @@ export function NewBlockCombobox() {
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-        <Button size="lg" variant="outline">
+        <Button size="lg" variant="outline" className="w-full">
           Add block <Plus className="ml-1 w-4 h-4" />
         </Button>
       </DrawerTrigger>
