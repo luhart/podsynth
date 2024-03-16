735e61f1f0608cd5689ec76c3c89bf042be1dd75
luhart <ljhartman10@gmail.com>
2024-03-15 17:49:22 -0700

    update

--- podsynth.com/components/workflow/WorkflowHeader.tsx
+++ podsynth.com/components/workflow/WorkflowHeader.tsx
@@ -11,30 +11,33 @@ export default function WorkFlowHeader() {
   const [title, setTitle] = useState("RSS → Audio Summary");
 
   return (
-    <div className="flex flex-row gap-2 items-center justify-between">
-      <div className="flex-grow">
-        <Input
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="font-semibold tracking-tight pr-0"
-          placeholder="Workflow"
-        />
-      </div>
+    <div>
+      <div className="text-base font-semibold tracking-tight mb-2 ">Workflow</div>
+      <div className="flex flex-row gap-2 items-center justify-between">
+        <div className="flex-grow">
+          <Input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="font-semibold tracking-tight pr-0"
+            placeholder="Workflow"
+          />
+        </div>
 
-      <div className="h-[1px] flex-1 bg-gray-200" />
-      <Button
-        variant={"outline"}
-        size={"sm"}
-        disabled={!blocks || blocks.length === 0}
-        onClick={() => {
-          setTitle("");
-          dispatch({
-            type: "RESET_WORKFLOW",
-          });
-        }}
-      >
-        Reset
-      </Button>
+        <div className="h-[1px] flex-1 bg-gray-200" />
+        <Button
+          variant={"outline"}
+          size={"sm"}
+          disabled={!blocks || blocks.length === 0}
+          onClick={() => {
+            setTitle("");
+            dispatch({
+              type: "RESET_WORKFLOW",
+            });
+          }}
+        >
+          Reset
+        </Button>
+      </div>
     </div>
   );
 }
